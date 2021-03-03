c7852cb51c2db5780ef5fbcb2f4e9e1d06c64b52
hdsz98 <zhouzc42@gmail.com>
2021-03-03 19:54:37 +0800

    21.3.3

---
+++ src/com/str/LeetCode0014.java
@@ -0,0 +1,21 @@
+package com.str;
+/*
+    easy
+        编写一个函数来查找字符串数组中的最长公共前缀
+ */
+public class LeetCode0014 {
+    public String longestCommonPrefix(String[] strs) {
+        int n = strs.length;
+        if (n == 0)
+            return "";
+        String res = strs[0];
+        for (int i = 1; i < n; i++){
+            while (!strs[i].startsWith(res)) {
+                if (res.length() == 0)
+                    return "";
+                res = res.substring(0, res.length() - 1);
+            }
+        }
+        return res;
+    }
+}
