84168b08c92d76e721b50f483642caa02c3290a4
hdsz98 <zhouzc42@gmail.com>
2021-03-11 21:30:41 +0800

    21.3.11

--- src/com/twoPointers/LeetCode0018.java
+++ src/com/twoPointers/LeetCode0018.java
@@ -6,21 +6,19 @@ import java.util.List;
 
 /*
     Medium
-        给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有和为 0 且不重复的三元组。
+        给定一个包含 n 个整数的数组 nums 和一个目标值 target，判断 nums 中是否存在四个元素 a，b，c 和 d ，使得 a + b + c + d 的值与 target 相等？找出所有满足条件且不重复的四元组。
 
-        注意：答案中不可以包含重复的三元组。
-
-        来源：力扣（LeetCode）
-        链接：https://leetcode-cn.com/problems/3sum
+        注意：答案中不可以包含重复的四元组。
  */
-public class LeetCode0015 {
+public class LeetCode0018 {
     List<List<Integer>> res = new ArrayList<>();
-    public void twoSum(int[] nums, int target, int left) {
+    public void twoSum(int[] nums, int first, int second, int target, int left) {
         int right = nums.length - 1;
         while (left < right) {
             if (nums[left] + nums[right] == target) {
                 List<Integer> temp = new ArrayList<Integer>();
-                temp.add(0 - target);
+                temp.add(first);
+                temp.add(second);
                 temp.add(nums[left]);
                 temp.add(nums[right]);
                 res.add(temp);
@@ -37,29 +35,37 @@ public class LeetCode0015 {
                 left++;
         }
     }
-    public List<List<Integer>> threeSum(int[] nums) {
-
+    public void threeSum(int[] nums, int first, int target, int left) {
+        int n = nums.length;
+        for (int i = left; i < n - 2; i++) {
+            if (nums[left] > 0 && nums[left] + first > target)
+                break;
+            if (i == left || (i > left && nums[i] != nums[i - 1])) {
+                twoSum(nums, first, nums[i], target - nums[i], i + 1);
+            }
+        }
+    }
+    public List<List<Integer>> fourSum(int[] nums, int target) {
         int n = nums.length;
         if (n < 3)
             return res;
         Arrays.sort(nums);
-        if (nums[0] > 0)
+        if (nums[0] > 0 && nums[0] > target)
             return res;
-        for (int i = 0; i < n - 2; i++) {
-            if (nums[i] > 0)
+        for (int i = 0; i < n - 3; i++) {
+            if (nums[i] > 0 && nums[i] > target)
                 break;
-            if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
-                int target = 0 - nums[i];
-                twoSum(nums, target, i + 1);
-            }
+            if (i == 0 || (i > 0 && nums[i] != nums[i - 1]))
+                threeSum(nums, nums[i], target - nums[i], i + 1);
         }
         return res;
     }
 
     public static void main(String[] args) {
-        int[] nums = {0, 0, 0};
+
+        int[] nums = {1,0,-1,0,-2,2};
         // int[] nums = {-1,0,1,2,-1,-4};
-        List<List<Integer>> res = new LeetCode0015().threeSum(nums);
+        List<List<Integer>> res = new LeetCode0018().fourSum(nums, 0);
         for (int i = 0; i < res.size(); i++) {
             for (int j = 0; j < res.get(i).size(); j++)
                 System.out.print(res.get(i).get(j) + " ");
